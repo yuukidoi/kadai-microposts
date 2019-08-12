0cd7b48022e94cfb20bd4e8f7182342e654d6c2a
yuuki.doi <dogrunruns@yahoo.co.jp>
2019-08-12 06:10:10 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,8 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました'
       redirect_to root_url
     else
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
+      
       flash[:danger] = '投稿に失敗しました'
       render 'toppages/index'
     end
