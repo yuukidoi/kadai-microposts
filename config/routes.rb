06985707e65f49420897f980eecf43fa329d97f4
yuuki.doi <dogrunruns@yahoo.co.jp>
2019-08-12 05:34:36 +0000

    follow,unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end 
+  end 
   
-  
-  resources :users, only: [:index, :show, :new, :create]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
