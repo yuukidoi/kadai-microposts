10f6c27acfde79f8ebebab1eb52ac0c63a346320
yuuki.doi <dogrunruns@yahoo.co.jp>
2019-08-14 01:11:55 +0000

    destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ApplicationRecord
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
-    has_many :favorites
-    has_many :likes, through: :favorites, source: :micropost
+    has_many :favorites, dependent: :destroy
+    has_many :likes, through: :favorites, source: :micropost, dependent: :destroy
     
     
     
