77794b4dc24e2ac8574c5ba2fb9d85fa18d26a06
yuuki.doi <dogrunruns@yahoo.co.jp>
2019-08-14 04:25:21 +0000

    destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,6 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255}
   
-  has_many :favorites
-  has_many :users, through: :favorites, source: :user
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites, source: :user, dependent: :destroy
 end
